98f5c31e70b801f1ff2317790f2563deda55162c
Fritz <fritz.developer10@gmail.com>
2024-01-16 22:35:53 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import ListaDeTareas from './componentes/ListaDeTareas'
+
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png"
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+        <div className='freecodecamp-logo-contenedor'>
+          <img src={freeCodeCampLogo} 
+          alt="logo de freecodecamp"
+          className='freecodecamp-logo' />
+        </div>
+        <div className='tareas-lista-principal'>
+          <h1>Mis Tareas</h1>
+          <ListaDeTareas/>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/ListaDeTareas.jsx
@@ -0,0 +1,38 @@
+import TareaFormulario from "./TareaFormulario";
+import { useState } from "react";
+import Tarea from "./Tarea";
+import "../hojas-de-estilo/ListaDeTareas.css"
+
+const ListaDeTareas = () => {
+    const [tareas,setTareas] = useState([]);
+
+    const agregarTarea = tarea => {
+        console.log(tarea);
+        if(tarea.texto.trim()){
+            tarea.texto = tarea.texto.trim();
+            const tareasActualizadas =[tarea, ...tareas];
+            setTareas(tareasActualizadas);
+        }
+    }
+
+  return (
+    <>
+        <TareaFormulario onSubmit={agregarTarea}/>
+        <div className="tarea-lista-contenedor">
+            {
+                tareas.map((tarea)=>{
+                    <Tarea
+                        key={tarea.id}
+                        id={tarea.id}
+                        texto={tarea.texto}
+                        completada={tarea.completada}
+                    />
+                }
+                )
+            }
+        </div>
+    </>
+  )
+}
+
+export default ListaDeTareas
